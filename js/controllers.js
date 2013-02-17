c83818b6245d8cba3887b983c1714e3bc3a827ed
neurolit <neurolit@gmail.com>
2013-02-17 22:45:20 +0100

    Génération de la liste des Grains à partir d'un objet JSON (via AngularJS)

---
+++ js/controllers.js
@@ -0,0 +1,25 @@
+    function SeedListCtrl($scope) {
+        $scope.seeds = [
+            {   "id": "carpasinivores",
+                "name": "Les carpasinivores",
+                "url": "http://inriamecsci.github.com/carpasinivores/"},
+            {   "id": "carpasinivores-limejs",
+                "name": "Les autres carpasinivores",
+                "url": "http://inriamecsci.github.com/carpasinivores-limejs/grain/"},
+            {   "id": "tetris-botte",
+                "name": "Le Tetris Botté",
+                "url": "http://inriamecsci.github.com/tetris-botte/"},
+            {   "id": "methodes-tri",
+                "name": "Les méthodes de tri",
+                "url": "https://iww.inria.fr/mecsci/grains3.0/methodes_de_tri/grain.html"},
+            {   "id": "machine-turing",
+                "name": "La machine de Turing",
+                "url": "https://iww.inria.fr/mecsci/grains3.0/turing/turing.html"},
+            {   "id": "automate-etats-finis-multiple-3",
+                "name": "Automate à états finis"},
+            {   "id": "voronoi-diagramme",
+                "name": "Diagramme de Voronoï"},
+            {   "id": "crepes",
+                "name": "Pile de crêpes "}
+        ];
+    }
