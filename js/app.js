540827147d07ab5f77edfc4cd99975aecc970cd2
neurolit <neurolit@gmail.com>
2013-03-06 12:05:57 +0100

    tags -> espaces + simplification appel config du module principal

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-    angular.module('grains', []).
-    	config(['$routeProvider', function($routeProvider) {
-    	$routeProvider.
-    		when('/grains', {templateUrl: 'partials/grains-list.html', controller: SeedListCtrl}).
-    		when('/grains/:seedId', {templateUrl: 'partials/grains-detail.html', controller: SeedDetailCtrl}).
-    		otherwise({redirectTo: '/grains'});
-    }]);
+angular.module('grains', []).
+    config(function($routeProvider) {
+        $routeProvider.
+            when('/grains', {templateUrl: 'partials/grains-list.html', controller: SeedListCtrl}).
+            when('/grains/:seedId', {templateUrl: 'partials/grains-detail.html', controller: SeedDetailCtrl}).
+            otherwise({redirectTo: '/grains'});
+});
