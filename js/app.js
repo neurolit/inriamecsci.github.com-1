25c10d0871879f15f3e397296e5c06f5656bcfac
neurolit <neurolit@gmail.com>
2013-03-06 12:07:01 +0100

    Passage en hashbangs (#!) pour les URLs de l'application

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 angular.module('grains', []).
-    config(function($routeProvider) {
+    config(function($routeProvider, $locationProvider) {
         $routeProvider.
             when('/grains', {templateUrl: 'partials/grains-list.html', controller: SeedListCtrl}).
             when('/grains/:seedId', {templateUrl: 'partials/grains-detail.html', controller: SeedDetailCtrl}).
             otherwise({redirectTo: '/grains'});
+        $locationProvider.hashPrefix('!');
 });
