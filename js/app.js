b8802fc45e3f7d4199b7e35c1c99bbfc8c5724d1
neurolit <neurolit@gmail.com>
2013-03-18 15:04:05 +0100

    Nouvelle page : la fiche signalétique d'un grain

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ angular.module('grains', []).
         $routeProvider.
             when('/grains', {templateUrl: 'partials/grains-list.html', controller: SeedListCtrl}).
             when('/grains/:seedId', {templateUrl: 'partials/grains-detail.html', controller: SeedDetailCtrl}).
+            when('/grains/meta/:seedId', {templateUrl: 'partials/grains-meta.html', controller: SeedDetailCtrl}).
             otherwise({redirectTo: '/grains'});
         $locationProvider.hashPrefix('!');
 });
