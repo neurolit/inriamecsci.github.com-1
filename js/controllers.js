c3cb1fa1eb6645c3bb2faf55983fac70ae730ab7
Neurolit <neurolit@gmail.com>
2014-03-23 23:42:33 +0100

    Minify et concat pour le JS
    * Nouvelles tâches Grunt : generer_js et generer_css
    * Nouvelles dépendances pour Grunt
    * Fonts "Awesome" copiées dans /fonts avec la tâche Grunt copy:fonts
    * Réécriture de l'app et des contrôleurs pour le minify

--- js/controllers.js
+++ js/controllers.js
@@ -1,25 +1,31 @@
-function SeedListCtrl($scope, $http) {
-    "use strict";
-    $http.get('grains/index.json').success(function (data) {
-        $scope.seeds = data;
-        $scope.seedsSlicesByThree = [];
-        var firstElement = 0;
-        while (firstElement < $scope.seeds.length) {
-            $scope.seedsSlicesByThree.push($scope.seeds.slice(firstElement, firstElement + 3));
-            firstElement = firstElement + 3;
-        }
-    });
-}
+/*global angular: false */
 
-function SeedDetailCtrl($scope, $routeParams, $http) {
-    "use strict";
-    $scope.seedId = $routeParams.seedId;
+var grainsControllers = angular.module('grainsControllers', []);
 
-    $http.get('grains/' + $routeParams.seedId + '/meta.json').success(function (data) {
-        $scope.seed = data;
-    });
+grainsControllers.controller('SeedListCtrl', [ '$scope', '$http',
+    function ($scope, $http) {
+        "use strict";
+        $http.get('grains/index.json').success(function (data) {
+            $scope.seeds = data;
+            $scope.seedsSlicesByThree = [];
+            var firstElement = 0;
+            while (firstElement < $scope.seeds.length) {
+                $scope.seedsSlicesByThree.push($scope.seeds.slice(firstElement, firstElement + 3));
+                firstElement = firstElement + 3;
+            }
+        });
+    }]);
 
-    $scope.getSeedUrl = function () {
-        return 'grains/' + $scope.seedId + '/index.html';
-    };
-}
+grainsControllers.controller('SeedDetailCtrl', [ '$scope', '$routeParams', '$http',
+    function ($scope, $routeParams, $http) {
+        "use strict";
+        $scope.seedId = $routeParams.seedId;
+
+        $http.get('grains/' + $routeParams.seedId + '/meta.json').success(function (data) {
+            $scope.seed = data;
+        });
+
+        $scope.getSeedUrl = function () {
+            return 'grains/' + $scope.seedId + '/index.html';
+        };
+    }]);
